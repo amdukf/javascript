4b94f504939ee7337394ff3b28b48deee3fc0788
amdukf <amdukf@gmail.com>
2022-03-31 12:58:28 +0430

    main

---
+++ test.js
@@ -0,0 +1,24 @@
+// expression function
+
+// var number = function() {
+//     var n1 = 3
+//     var n2 = 5
+//     alert("addition is " + n1+n2)
+// }
+
+// number()
+
+//------//
+
+// default variable for a function
+
+// var number = function(num1 = 1 , num2 = 2){
+//     alert(num1+num2)
+// }
+// number(10)
+
+//------//
+
+
+
+
